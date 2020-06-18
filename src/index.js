ef8b02fca1fb13c01466d6a4fedbe651610842b2
Ruslan Kamaleev <44605327+Kamaleev@users.noreply.github.com>
2020-06-19 01:00:53 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-// import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 import './styles.css';
